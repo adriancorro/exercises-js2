b89cedbb55e6abf8befc77bf37c14a85017a2859
adriancorro <adriancorrog@gmail.com>
2021-04-04 10:19:16 +0200

    desarrollando falta homework slides show

--- week-3/Homework/1-alarmclock/alarmclock.js
+++ week-3/Homework/1-alarmclock/alarmclock.js
@@ -1,4 +1,41 @@
-function setAlarm() {}
+let i = -1;
+let interval = 0 ;
+function setAlarm() {
+
+ clearInterval(i)
+ let value = document.querySelector("#alarmSet").value;
+ let change = 1000; 
+
+i = setInterval (  () =>  {
+  let time = document.querySelector("#timeRemaining");
+  if( value === 0 ){
+    clearInterval(i)
+    playAlarm()
+  }
+
+  let min = parseInt(value/60)  < 10  ?  ` 0${ parseInt(  value  / 60) }`    : parseInt (value/60) ;
+  let seg = value % 60 < 10 ? ` 0${value % 60 }` :  value % 60 
+  time.innerHTML = ` time remaining: ${min}: ${seg}`
+  value = value -1
+
+ 
+  
+}, change);
+}
+
+let stop2 = document.querySelector("#stop");
+stop2.addEventListener( "click" ,  stopAlarm)
+
+ function stopAlarm (){
+  
+   clearInterval(i);
+   
+ 
+   }
+
+
+
+
 
 // DO NOT EDIT BELOW HERE
 
